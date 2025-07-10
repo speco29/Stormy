4eac5d222d9e0ed9e49682ce912884ca1c3d00b9
specsy <sdiya5556@gmail.com>
2025-07-10 23:01:56 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
+ 
 contextBridge.exposeInMainWorld('electronAPI', {
   navigate: (page) => ipcRenderer.send('navigate', page),
   controlWindow: (action) => ipcRenderer.send('window-control', action),
